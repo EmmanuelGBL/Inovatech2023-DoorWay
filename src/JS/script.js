b91b263ebc476738ab2999226bfb536dcf2a0060
EmmanuelGBL <emmanuelgbl@outlook.com>
2023-05-20 18:58:42 -0300

    Inovatech1.0

---
+++ src/JS/script.js
@@ -0,0 +1,154 @@
+let texto = " ninguém será submetido a tortura nem a tratamento desumano ou degradante;";
+document.getElementById("mensagem").innerHTML = `<i> ${texto}`;
+
+let texto2 = " é livre a manifestação do pensamento, sendo vedado o anonimato ";
+document.getElementById("mensagem2").innerHTML = `<i> ${texto2}`;
+
+let texto3 = " é assegurado o direito de resposta, proporcional ao agravo, além da indenização por dano material, moral ou à imagem; ";
+document.getElementById("mensagem3").innerHTML = `<i> ${texto3}`;
+
+let texto4 = " é inviolável a liberdade de consciência e de crença, sendo assegurado o livre exercício dos cultos religiosos e garantida, na forma da lei, a proteção aos locais de culto e a suas liturgias; ";
+document.getElementById("mensagem4").innerHTML = `<i> ${texto4}`;
+
+let texto5 = " é assegurada, nos termos da lei, a prestação de assistência religiosa nas entidades civis e militares de internação coletiva; ";
+document.getElementById("mensagem5").innerHTML = `<i> ${texto5}`;
+
+let texto6 = " ninguém será privado de direitos por motivo de crença religiosa ou de convicção filosófica ou política, salvo  ";
+document.getElementById("mensagem6").innerHTML = `<i> ${texto6}`;
+
+let texto7 = " é livre a expressão da atividade intelectual, artística, científica e de comunicação, independentemente ";
+document.getElementById("mensagem7").innerHTML = `<i> ${texto7}`;
+
+let texto8 = " a casa é asilo inviolável do indivíduo, ninguém nela podendo penetrar sem consentimento do morador, salvo em c ";
+document.getElementById("mensagem8").innerHTML = `<i> ${texto8}`;
+
+let texto9 = " é inviolável o sigilo da correspondência e das comunicações telegráficas, de dados e das comunicações telefônicas ";
+document.getElementById("mensagem9").innerHTML = `<i> ${texto9}`;
+
+let texto10 = " é livre o exercício de qualquer trabalho, ofício ou profissão, atendidas as qualificações profissionais que a lei estabelecer";
+document.getElementById("mensagem10").innerHTML = `<i> ${texto10}`;
+
+
+
+
+function abrirModal() {
+    const modal = document.getElementById('janela-modal')
+    modal.classList.add('abrir')
+
+    modal.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal') {
+            modal.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal2() {
+    const modal2 = document.getElementById('janela-modal2')
+    modal2.classList.add('abrir')
+
+    modal2.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal2') {
+            modal2.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal3() {
+    const modal3 = document.getElementById('janela-modal3')
+    modal3.classList.add('abrir')
+
+    modal3.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal3') {
+            modal3.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal4() {
+    const modal4 = document.getElementById('janela-modal4')
+    modal4.classList.add('abrir')
+
+    modal4.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal4') {
+            modal4.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal5() {
+    const modal5 = document.getElementById('janela-modal5')
+    modal5.classList.add('abrir')
+
+    modal5.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal5') {
+            modal5.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal6() {
+    const modal6 = document.getElementById('janela-modal6')
+    modal6.classList.add('abrir')
+
+    modal6.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal6') {
+            modal6.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal7() {
+    const modal7 = document.getElementById('janela-modal7')
+    modal7.classList.add('abrir')
+
+    modal7.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal7') {
+            modal7.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal8() {
+    const modal8 = document.getElementById('janela-modal8')
+    modal8.classList.add('abrir')
+
+    modal8.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal8') {
+            modal8.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal9() {
+    const modal9 = document.getElementById('janela-modal9')
+    modal9.classList.add('abrir')
+
+    modal9.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal9') {
+            modal9.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+function abrirModal10() {
+    const modal10 = document.getElementById('janela-modal10')
+    modal10.classList.add('abrir')
+
+    modal10.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-modal10') {
+            modal10.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+
