c57789264dfa65f796d818aa8523e5b5cd01f2df
EmmanuelGBL <emmanuelgbl@outlook.com>
2023-05-23 02:15:39 -0300

    Inovatech2.0

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,34 +1,35 @@
-let texto = " ninguém será submetido a tortura nem a tratamento desumano ou degradante;";
+let texto = "O CEJUR é o Centro de Estudos Jurídicos localizado no campus universitário. É um espaço dedicado ao aprimoramento acadêmico e profissional no campo do Direito. Oferece cursos, workshops e palestras ministradas por renomados professores e especialistas. O CEJUR também promove a pesquisa jurídica, disponibilizando recursos como biblioteca especializada e laboratórios equipados.";
 document.getElementById("mensagem").innerHTML = `<i> ${texto}`;
 
-let texto2 = " é livre a manifestação do pensamento, sendo vedado o anonimato ";
+let texto2 = " A Reitoria/Secretaria é o centro administrativo da Universidade, oferecendo uma variedade de serviços e informações essenciais para estudantes, professores e funcionários.";
 document.getElementById("mensagem2").innerHTML = `<i> ${texto2}`;
 
-let texto3 = " é assegurado o direito de resposta, proporcional ao agravo, além da indenização por dano material, moral ou à imagem; ";
+let texto3 = " Localizado dentro da faculdade, o ambulatório é um espaço dedicado a fornecer cuidados médicos de qualidade aos estudantes, professores e funcionários da instituição.";
 document.getElementById("mensagem3").innerHTML = `<i> ${texto3}`;
 
-let texto4 = " é inviolável a liberdade de consciência e de crença, sendo assegurado o livre exercício dos cultos religiosos e garantida, na forma da lei, a proteção aos locais de culto e a suas liturgias; ";
+let texto4 = "Bem-vindo(a) à Praça de Alimentação da Faculdade! Aqui, você encontrará uma variedade de opções gastronômicas para satisfazer seu paladar e desfrutar de refeições deliciosas e equilibradas, com dois restaurantes incluso.";
 document.getElementById("mensagem4").innerHTML = `<i> ${texto4}`;
 
-let texto5 = " é assegurada, nos termos da lei, a prestação de assistência religiosa nas entidades civis e militares de internação coletiva; ";
+let texto5 = " As gráficas sao um destino confiável para impressão, cópias, digitação, encadernação e para confecções de camisas personalizadas com estilo universitário. Localizadas no campus, oferecem serviços de alta qualidade e personalizados. Com tecnologia avançada, as gráficas garantem resultados nítidos e acabamento impecáveis.";
 document.getElementById("mensagem5").innerHTML = `<i> ${texto5}`;
 
-let texto6 = " ninguém será privado de direitos por motivo de crença religiosa ou de convicção filosófica ou política, salvo  ";
+let texto6 = "Bem-vindo(a) à quadra esportiva da faculdade! O espaço oferece oportunidades para a prática de atividades físicas, esportes e momentos de lazer. Desfrute de um ambiente saudável e estimulante, onde você pode se exercitar, socializar e relaxar.";
 document.getElementById("mensagem6").innerHTML = `<i> ${texto6}`;
 
-let texto7 = " é livre a expressão da atividade intelectual, artística, científica e de comunicação, independentemente ";
+let texto7 = "Bem-vindo(a) ao Miniauditório da Faculdade! O espaço foi projetado para proporcionar um ambiente adequado para palestras, apresentações, seminários e eventos acadêmicos. Com capacidade para acomodar um número menor de pessoas, oferecemos um local intimista e acolhedor para suas atividades acadêmicas.";
 document.getElementById("mensagem7").innerHTML = `<i> ${texto7}`;
 
-let texto8 = " a casa é asilo inviolável do indivíduo, ninguém nela podendo penetrar sem consentimento do morador, salvo em c ";
+let texto8 = " ndo penetrar sem consentimento do morador, salvo em c ";
 document.getElementById("mensagem8").innerHTML = `<i> ${texto8}`;
 
-let texto9 = " é inviolável o sigilo da correspondência e das comunicações telegráficas, de dados e das comunicações telefônicas ";
+let texto9 = " ée das comunicações telegráficas, de dados e das comunicações telefônicas ";
 document.getElementById("mensagem9").innerHTML = `<i> ${texto9}`;
 
-let texto10 = " é livre o exercício de qualquer trabalho, ofício ou profissão, atendidas as qualificações profissionais que a lei estabelecer";
+let texto10 = "  profissionais que a lei estabelecer";
 document.getElementById("mensagem10").innerHTML = `<i> ${texto10}`;
 
-
+let textoInstrucao = "Lorem ipsum, dolor sit amet Lorem ipsum, dolor sit ametLorem ipsum, dolor sit ametLorem ipsum, dolor sit ametLorem ipsum, dolor sit ametLorem ipsum, dolor sit ametLorem ipsum, dolor sit amet consectetur adipisicing elit. Autem quos eius excepturi repellendus fuga, aut maxime repellat, at aliquid nostrum culpa! Dolore iste dolores officia doloremque fugiat nemo provident sed."
+document.getElementById("mensagem-Instrucao").innerHTML = `<i> ${textoInstrucao}`
 
 
 function abrirModal() {
@@ -151,4 +152,58 @@ function abrirModal10() {
     )
 }
 
+$('#image-map area').hover(
+	function () { 
+		var coords = $(this).attr('coords').split(',');
+		var width = $('.map-class').width();
+		var height = $('.map-class').height();
+		$('.map-class .map-selector').addClass('hover').css({
+			'left': coords[0]+'px',
+			'top': coords[1] + 'px',
+			'right': width - coords[2]+'px',
+			'bottom': height - coords[3]
+		})
+	 },
+	function () { 
+		$('.map-class .map-selector').removeClass('hover').attr('style','');
+	}
+)
 
+
+function abrirInstrucao() {
+    const instruction = document.getElementById('janela-Instrucao')
+    instruction.classList.add('abrir')
+
+    instruction.addEventListener('click', (e) => {
+        if (e.target.id == 'fechar' || e.target.id == 'janela-Instrucao') {
+            instruction.classList.remove('abrir')
+        }
+    }
+    )
+}
+
+document.addEventListener("DOMContentLoaded", function(event) {
+    let slide = document.querySelectorAll(".slide");
+    let currentSlide = 0;
+    const slideInterval = setInterval(nextSlide, 5000);
+  
+    function nextSlide() {
+      slide[currentSlide].style.display = "none";
+      currentSlide = (currentSlide + 1) % slide.length;
+      slide[currentSlide].style.display = "block";
+    }
+  });
+  
+  document.addEventListener("DOMContentLoaded", function(event) {
+    let datashow = document.querySelectorAll(".passar-foto");
+    let currentSlide = 0;
+    const slideInterval = setInterval(nextSlide, 5000);
+  
+    function nextSlide() {
+      datashow[currentSlide].style.display = "none";
+      currentSlide = (currentSlide + 1) % datashow.length;
+      datashow[currentSlide].style.display = "block";
+    }
+  });
+  
+  
